78b44476c20a5bed152e6b759c405226a4700935
s7electric <samiacobazzi@gmail.com>
2026-05-23 22:43:00 -0400

    wip

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+import torch
+
+print(torch.cuda.is_available())
+
+
+
+# class Model(nn.Module):
+#     def __init__(self, in_features, out_features):
+#         super().__init__()
