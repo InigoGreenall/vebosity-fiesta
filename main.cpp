abd869ac67c41a83ee04e4bc8e84ce22681e2c94
s7electric <samiacobazzi@gmail.com>
2026-06-04 18:01:27 -0400

    implement NN model class and tensor logic + example usage

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "main.hpp"
 #include "dtypes.hpp"
+#include "model.hpp"
 #include "softwarerender.hpp"
-#include <array>
+#include <unistd.h>
+
 extern "C" {
     #include "testing-include/wlclient.h"
 }
@@ -14,15 +16,59 @@ int main() {
 
     // TODO: do program setup
     entity_map = new EntityMap;
-    entity_map->entities.push_back(new Entity(200, 200, 100, 0, 0, 0));
+    entity_map->entities.push_back(
+        new Entity(WIDTH/2, HEIGHT/2, 50, 1, 1, 1, Net({7, 64, 64, 2}, torch::kCUDA))
+    );
 
     while (dispatch_events(state)) {
 
         // TODO: event loop
 
-        entity_map->entities[0]->x += 1;
+        Vec2 out = entity_map->entities[0]->query_model();
+        entity_map->entities[0]->x += out.x;
+        entity_map->entities[0]->x += out.y;
 
         request_new_frame(state);
+        sleep(1);
     }
     return 0;
 }
+
+
+// torch::DeviceType device = torch::kCUDA;
+// int main() {
+//     Net* n = new Net({1, 1});
+//     n->to(device);
+//     float learning_rate = 0.01;
+//     auto optimizer = torch::optim::Adam(n->parameters(), learning_rate);
+//     auto loss_func = torch::nn::MSELoss();
+//     int epochs = 4000;
+    
+//     torch::Tensor x = torch::rand({200, 1}).to(device);
+//     torch::Tensor correct = torch::multiply(x, 2).to(device);
+//     for (int i = 0; i < epochs; i++) {
+//         optimizer.zero_grad();
+//         torch::Tensor y = n->forward(x);
+//         auto loss = loss_func(y, correct);
+//         if (i % 10 == 0) {
+//             std::cout << "###################\n";
+//             std::cout << "input: \n" << x << "\n";
+//             std::cout << "prediction: \n" << y << "\n";
+//             std::cout << "loss: " << loss.item() << "\n";
+//             std::cout << "###################\n";
+//         }
+//     loss.backward();
+//     optimizer.step();
+//     }
+
+//     std::cout << "----- TEST SECTION -----\n";
+//     for (int i = 0; i < 4; i++) {
+//         std::cout << "TEST " << i << ":\n";
+//         torch::Tensor testX = torch::rand({1,1}).to(device);
+//         torch::Tensor testY = torch::multiply(testX, 2).to(device);
+//         std::cout << "testX:\n" << testX << "\n";
+//         std::cout << "testY:\n" << testY << "\n";
+//         std::cout << "prediction:\n" << n->forward(testX) << "\n";
+//     }
+//     return 0;
+// }
--- model.cpp
+++ model.cpp
@@ -1,4 +1,34 @@
-#include <torch/torch.h>
-#include <torch/csrc/api/include/nn.h>
+#include "model.hpp"
+#include <vector>
 
-class model : public torch::model
+Net::Net() : 
+    Net({15, 64, 64, 2}, torch::kCUDA)
+{}
+
+Net::Net(std::vector<int> layer_depths, torch::DeviceType device):
+    device(device) {
+    for (int i = 0; i < layer_depths.size()-1; i++) {
+        layers.push_back(register_module(
+            "layer"+std::to_string(i),
+            torch::nn::Linear(layer_depths[i], layer_depths[i+1])
+        ));
+    }
+    this->to(device);
+}
+
+torch::Tensor Net::forward(torch::Tensor x) {
+    for (int i = 0; i < layers.size()-1; i++) {
+        x = torch::relu(layers[i]->forward(x));
+    }
+    x = layers[layers.size()-1]->forward(x);
+    return x;
+}
+
+std::vector<float> Net::make_prediction(std::vector<float> input) {
+    torch::Tensor t_input = torch::tensor(input).clone().to(device);
+    torch::Tensor t_output = forward(t_input);
+    t_output.contiguous();
+    return std::vector<float>(
+        {t_output.index({0}).item<float>(), t_output.index({1}).item<float>()}
+    );
+}
